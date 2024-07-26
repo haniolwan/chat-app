ad87f2ebdf9cc9518f3e5adcd89868b2d658c699
Issam Olwan <lands.of.48@gmail.com>
2024-07-27 00:24:42 +0300

    edit layout.. again

--- src/components/sidebar/messages/message-item/message-item.jsx
+++ src/components/sidebar/messages/message-item/message-item.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const MessageItem = ({ name, message, time, avatar, statusIcon, unreadCount }) => (
-  <div className="grid grid-cols-12 gap-1 p-4 items-center border-b border-gray-700">
+  <div className="grid grid-cols-12 gap-1 p-2 items-center border-b border-gray-700">
     <div className="col-span-2">
       <img src={avatar} alt={`${name} avatar`} className="w-12 h-12 rounded-full" />
     </div>
@@ -12,12 +12,12 @@ const MessageItem = ({ name, message, time, avatar, statusIcon, unreadCount }) =
       </div>
     </div>
     <div className="col-span-1 text-right">
-      <span className="text-gray-400 text-sm">{time}</span>
+      <span className="text-gray-400 text-sm relative top-2">{time}</span>
     </div>
     <div className="col-span-2 flex justify-end items-center space-x-2">
       {statusIcon && <FontAwesomeIcon icon={statusIcon} className="text-gray-500" />}
       {unreadCount > 0 && (
-        <div className="bg-blue-500 text-white text-xs rounded w-10 h-6 flex items-center justify-center">
+        <div className="bg-blue-500 text-white text-xs rounded w-10 h-6 flex items-center justify-center relative top-2">
           {unreadCount}
         </div>
       )}
